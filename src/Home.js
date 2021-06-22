3b707c769f0287a09fb0a84954adfc5e97f12c94
armathur <armathur@iu.edu>
2021-06-22 19:17:56 -0400

    #7 Click Event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('Hello idiots');
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hello ' + name);
+    }
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick = {handleClick}>CLick me</button>
+            <button onClick = {() => {
+                handleClickAgain('mario')
+            }}></button>
         </div>
     );
 }
