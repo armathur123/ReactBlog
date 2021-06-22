be85073334395177110b921b8080962351ec0b3d
armathur <armathur@iu.edu>
2021-06-22 18:37:22 -0400

    #5 dynamic values  in templates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  const person = {name :'arjun', age: 21};
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <p>{ 10 }</p>
+        <p>{"hello fools"}</p>
       </div>
     </div>
   );
