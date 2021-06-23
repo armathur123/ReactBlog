ea6a6a86c33f1d31de320a01e83ab3d630680438
armathur <armathur@iu.edu>
2021-06-22 20:56:34 -0400

    #8 Hooks intro

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,18 @@
+import { useState } from 'react';
+
 const Home = () => {
-
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+    
     const handleClick = () => {
-        console.log('Hello idiots');
-    }
-
-    const handleClickAgain = (name) => {
-        console.log('hello ' + name);
+        setName('luigi');
+        setAge('30');
     }
     return (
         <div className="home">
             <h2>Homepage</h2>
-            <button onClick = {handleClick}>CLick me</button>
-            <button onClick = {() => {
-                handleClickAgain('mario')
-            }}></button>
+            <p>{ name } is {age} years old</p>
+            <button onClick = {() => { handleClick() }}>Click me</button>
         </div>
     );
 }
