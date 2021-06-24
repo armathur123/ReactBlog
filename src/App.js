20d44e5233fad72614a8d305bf9b751ed6b8cb0a
armathur <armathur@iu.edu>
2021-06-24 15:21:00 -0400

    #21 Routes and multiple pages setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import Navbar from './navbar'
-import Home from './Home'
+import Navbar from './navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/> {/*self closing*/}
-      <div className="content">
-        <Home></Home>
+    <Router>
+      <div className="App">
+        <Navbar/> {/*self closing tag*/}
+        <div className="content">
+          <Switch>
+            <Route path = "/">
+              <Home></Home> {/*route to home component when path is "/"*/}
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
