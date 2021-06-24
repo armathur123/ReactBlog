11ce0760420d15b18a48b1daffb9c1a4eddf2aa3
armathur <armathur@iu.edu>
2021-06-23 20:48:23 -0400

    #19 handling errors

--- src/Home.js
+++ src/Home.js
@@ -5,27 +5,34 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         /*get request, cant use async on this statement, but could use on whole function*/
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
                 .then(res => {
+                    if (!res.ok) {
+                        throw Error('could not fetch data for that resource!');
+                    }
                     return res.json(); /*passes json into a js object*/
                 })
                 .then((data) => {
                     setBlogs(data);
                     setIsPending(false);
+                    setError(null);
                 })
                 .catch((e) => {
-                    console.log(e.message);
+                    setIsPending(false);
+                    setError(e.message);
                 })
         }, 1000)
     }, [])
 
     return (
         <div className="home">
-            {/*example(s) of a prop*/}
+            {/*example(s) of a prop and conditional statement*/}
+            {error && <div> {error} </div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="Arjuns blogs!" />}
         </div>
